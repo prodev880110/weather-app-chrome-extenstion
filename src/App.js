fd0e7df48ab869da7fd29b937808a8034397fac7
prodev880110 <jack.dev880110@gmail.com>
2020-10-22 11:14:17 -0500

    removed .env and add .env-example file

--- src/App.js
+++ src/App.js
@@ -3,18 +3,9 @@ import HomePage from "./components/HomePage"
 import './App.css'
 import { KeywordStore } from "./store"
 import Navbar from "react-bootstrap/Navbar"
-import { Security, ImplicitCallback, SecureRoute } from "@okta/okta-react"
-import Router, { Link, goBack } from 'route-lite';
-import { BrowserRouter, Route } from 'react-router-dom'
-import Login from "./components/auth/Login"
+import Router, { Link } from 'route-lite';
+import { BrowserRouter } from 'react-router-dom'
 const keywordStore = new KeywordStore()
-
-const config = {
-  issuer: 'https://dev-979265.okta.com/oauth2/default',
-  redirectUri: `${window.location.origin}/implicit/callback`,
-  clientId: '0oa3abxqyBJhNeYBK4x6',
-  pkce: true,
-}
 
 function App() {
   return (
@@ -26,14 +17,8 @@ function App() {
               <Navbar.Brand href="#home">Weather App</Navbar.Brand>
             </Navbar>
             <Link component={HomePage} componentProps={{ store: keywordStore }}>
-              {/* <HomePage keywordStore={keywordStore} /> */}
               Weather
             </Link>
-            <Link component={Login} componentProps={{ baseUrl: 'https://dev-979265.okta.com' }}>
-              Login
-            </Link>
-            <Route path='/implicit/callback' component={ImplicitCallback} />
-
           </div>
         </Security>
       </Router>
